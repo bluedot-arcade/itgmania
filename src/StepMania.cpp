d5f31da3f44f334fe8ecbbbec36d5dabbebd1949
dando92 <alessandro.dore@live.it>
2025-10-21 14:53:50 +0200

    Cherry picked syncstart from alhetus repo and added windows impl

--- src/StepMania.cpp
+++ src/StepMania.cpp
@@ -69,6 +69,7 @@
 #include "Profile.h"
 #include "ActorUtil.h"
 #include "ver.h"
+#include "SyncStartManager.h"
 
 #include <cmath>
 #include <ctime>
@@ -960,6 +961,7 @@ int sm_main(int argc, char* argv[])
 	SONGMAN->UpdatePreferredSort();
 	NETWORK		= new NetworkManager;
 	STATSMAN	= new StatsManager;
+	SYNCMAN		= new SyncStartManager;
 
 	// Initialize which courses are ranking courses here.
 	SONGMAN->UpdateRankingCourses();
